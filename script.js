d1e713005f2677bf593b88bd41c92efaab195cac
exoki <77338136+exoki@users.noreply.github.com>
2021-05-28 19:35:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-let secretNumber=getRandomInt(0,9);
-let attempts=3;
-function getRandomInt(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-document.querySelector(".check").onclick=function(){
-    let userNumber=document.querySelector(".number").value;
-    //console.log(userNumber==secretNumber);
-    //console.log(userNumber<secretNumber);
-    //console.log(userNumber>secretNumber);
-    if (userNumber==secretNumber){
-        document.querySelector(".hint").innerHTML="Вы угадали!";
+let operators = ["+", "-", "*", "/", "."];
+document.querySelector(".buttons").onclick = function () {
+    let target = event.target;
+    let input = document.querySelector("#expression")
+    let value = "";
+    if (target.classList.contains("number")) {
+        value = target.innerHTML;
+        if (input.value == "0"){
+            input.value = value;
+        } else {
+            input.value += value;
+        }
+    } else if (target.classList.contains("operation")) {
+        value = target.innerHTML;
+        let expression = input.value;
+        //получаем последний символ из выражения
+        let lastChar = expression[expression.length - 1];
+        if (operators.indexOf(lastChar) != -1) {
+            //заменяем последний символ в выражении на новый символ 
+            //.replace непосредственно заменяет 
+            //в скобках первый параметр - это регулярное выражение
+            //второй параметр - новое значение
+            expression = expression.replace(/.$/, value);
+            input.value = expression;
+        } else {
+            // сокращенная запись от
+            //input.value = input.value + target.innerHTML
+            input.value += value;
+        }
+    } else if (target.classList.contains("calculation")) {
+        let expression = input.value;
+        let result = eval(expression);
+        input.value = result;
+    } else if (target.classList.contains("clearOne")) {
+        let expression = input.value;
+        expression = expression.substring(0, expression.length - 1);
+        input.value = expression;
+    } else if (target.classList.contains("clear")) {
+        input.value = "0";
     }
-    if (userNumber>secretNumber){
-        document.querySelector(".hint").innerHTML="Попробуй число поменьше";
-    }
-    if (userNumber<secretNumber){
-        document.querySelector(".hint").innerHTML="Попробуй число побольше";
-    }
-    attempts=attempts - 1;
-    document.querySelector(".attempts").innerHTML=attempts;
-    if (attempts==0){
-        document.querySelector(".number").disabled=true;
-    document.querySelector(".check").disabled=true;
-    }
-};
-document.querySelector(".start").onclick=function(){
-    secretNumber=getRandomInt(0,9);
-    attempts=3;
-    document.querySelector(".hint").innerHTML="Я буду подсказывать";
-    document.querySelector(".attempts").innerHTML=attempts;
-    document.querySelector(".number").disabled=false;
-    document.querySelector(".check").disabled=false;
-    document.querySelector(".number").value="";
-    document.querySelector(".number").focus();
 }
